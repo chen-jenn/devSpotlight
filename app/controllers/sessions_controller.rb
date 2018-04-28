6498e008f2b3b2aa072aa4b7394fe98bb1e77017
Hercilia Diniz <herciliadiniz@gmail.com>
2018-04-28 14:10:27 -0700

    Create Session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.authenticate(params[:session][:password])
+      flash[:notice] = "Thank you for signing in!"
+      session[:user_id] = user.id
+      redirect_to home_path
+    else
+      flash.now[:alert] = "Something went wrong. Please check your email or password!"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Signed out!"
+    redirect_to home_path
+  end
+
 end
