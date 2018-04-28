63eb94d7f730789d6be992a12a149ea4c2ef4d66
Hercilia Diniz <herciliadiniz@gmail.com>
2018-04-28 15:32:22 -0700

    Fix validations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ApplicationRecord
   has_many :users
   has_many :tech_stacks
   has_many :events
-  has_many :technologies, through :tech_stacks
+  has_many :technologies, through: :tech_stacks
 
   validates :name, presence: true, uniqueness: true
 end
