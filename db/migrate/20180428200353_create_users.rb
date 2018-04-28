2a42c263cdf9585036c0f0c0a8fb50756cf8ef70
Cedric Chou <cdric.chou@gmail.com>
2018-04-28 13:11:12 -0700

    database done

---
+++ db/migrate/20180428200353_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.boolean :approved
+      t.integer :permission_type
+      t.string :password_digest
+      t.references :organization, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
