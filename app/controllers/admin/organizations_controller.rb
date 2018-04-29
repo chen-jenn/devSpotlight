6a3e2497e28e79fc42a709cbb0d5f2d902a78f03
Jennifer Chen <jenn.chen@live.ca>
2018-04-28 17:46:22 -0700

    Admin controllers (#13)
    * Add session verification in application.html.erb
    
    * Create ability.rb
    
    * Create admin controllers
    
    * Add admin authorization in admin-controllers
    
    * Add admin panel user views
    
    * Fix seed file to allow user password
    
    * Add default organization_id for user sign up
    
    * Change ability

--- app/controllers/admin/organizations_controller.rb
+++ app/controllers/admin/organizations_controller.rb
@@ -1,15 +1,9 @@
 class Admin::OrganizationsController < ApplicationController
-  before_action :authenticate_user! #is logged in
-  before_action :authorize_admin! # and is also admin
+  before_action :authenticate_user!
+  before_action :authorize_admin!
 
-  def index
+  def index #probably dont need this action either, unless to display more detailed information 
     @organizations = Organization.order(name: :asc)
-  end
-
-  def new
-  end
-
-  def create
   end
 
   def edit
@@ -21,15 +15,13 @@ class Admin::OrganizationsController < ApplicationController
   def destroy
   end
 
-  # def show
-  #   # might not need
-  # end
-
   private
   def authorize_admin!
     unless current_user.permission_type == 3
-      flas[:alert] = "Access Denied"
+      flash[:alert] = "Access Denied"
       redirect_to home_path
     end
   end
+
+
 end
