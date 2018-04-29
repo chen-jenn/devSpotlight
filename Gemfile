ab8edba998386127bf778aa86ed5bfb5a2ebe311
Cedric Chou <cdric.chou@gmail.com>
2018-04-29 12:24:36 -0700

    news api implemented - needs cosmetics

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'underscore-rails'
 gem 'jquery-rails'
 gem 'carrierwave', '~> 1.0'
 gem "mini_magick"
+gem 'news-api'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,6 +1,18 @@
 class AnnouncementsController < ApplicationController
 
+  require 'news-api'
+
   def index
+    @newsapi = News.new('c5d28ab09641461793d60c537e0a0466')
+    @all_articles = @newsapi.get_everything(q: 'bitcoin',
+                                      sources: 'bbc-news,the-verge',
+                                      domains: 'bbc.co.uk,techcrunch.com',
+                                      from: '2018-04-01',
+                                      to: '2018-04-25',
+                                      language: 'en',
+                                      sortBy: 'relevancy',
+                                      page: 1)
+
     @announcements = Announcement.order(created: :desc)
   end
 
