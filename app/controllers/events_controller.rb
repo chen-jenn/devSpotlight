afcb7f5dbcbcfb65a11f262045d689fe2eab85b3
Yousef Kbeili <ykbeili@gmail.com>
2018-04-29 11:15:18 -0700

    Working on events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,14 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.order(created_at: :desc)
+    params = { category: '1',
+        city: 'Vancouver',
+        country: 'Canada',
+        status: 'upcoming',
+        format: 'json',
+        page: '20'}
+      meetup_api = MeetupApi.new
+      @events = meetup_api.open_events(params)
   end
 
 end
