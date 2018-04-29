6c2e4c6627c47f64d2b3274180785ee95586feea
Cedric Chou <cdric.chou@gmail.com>
2018-04-29 15:52:31 -0700

    display list of companies on directory page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180429184556) do
+ActiveRecord::Schema.define(version: 20180429222023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20180429184556) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "tech_stacks", force: :cascade do |t|
