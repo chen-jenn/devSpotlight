14a2f4eaacd68ae3c394a6bbba2bc727e409b7d6
jennifer <jenn.chen@live.ca>
2018-04-28 17:44:12 -0700

    Change ability

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,14 @@ Rails.application.routes.draw do
   resources :announcements
 
   namespace :admin do
-    resources :organizations
-    resources :users, only: [:index, :destroy]
+    resources :organizations, only: [:index]
+    resources :users
     resources :events
     resources :technologies
     resources :announcements
   end
 
   get('/', { to: 'welcome#index', as: 'home' })
-  get('/about', { to: 'welcome#about'} )
+  get('/about', { to: 'welcome#show'} )
 
 end
